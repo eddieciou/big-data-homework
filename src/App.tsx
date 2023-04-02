af6fa275c83375c6e208f8db25b4b529effd42f4
eddieciou <a589658gur@gmail.com>
2023-04-02 13:07:05 +0800

    建立共用header

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import { AiOutlineSetting } from 'react-icons/ai';
+
 import HomePage from './containers/HomePage';
 import ResultPage from './containers/ResultPage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/:year/:county/:town" element={<ResultPage />} />
-    </Routes>
+    <>
+      <header className="flex h-12 items-center justify-between bg-[#651FFF] px-4">
+        <div className="text-base font-bold text-white">Logo</div>
+        <div className="rounded-md border border-[#8f60f5] p-1"><AiOutlineSetting className="text-base text-white" /></div>
+      </header>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/:year/:county/:town" element={<ResultPage />} />
+      </Routes>
+    </>
   );
 }
 
