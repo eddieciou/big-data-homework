137190601ee31ef61cecb635434463a308e86c5d
eddieciou <a589658gur@gmail.com>
2023-04-04 22:50:11 +0800

    import react-chartjs-2 製作bar chart

--- src/containers/ResultPage.tsx
+++ src/containers/ResultPage.tsx
@@ -1,7 +1,26 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import useSWR from 'swr';
-import { select } from 'd3';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+);
 
 type TAPIResult = {
   responseData:Array<{
@@ -16,14 +35,10 @@ type TAPIResult = {
 
 const fetcher = (url:string) => fetch(url).then((r) => r.json());
 
-const svgData = [25, 30, 45, 60, 20];
-
 function ResultPage() {
   const { year, county, town } = useParams();
 
   const { data, isLoading } = useSWR<TAPIResult>(() => `https://www.ris.gov.tw/rs-opendata/api/v1/datastore/ODRP019/${year}?COUNTY=${county}&TOWN=${town}`, fetcher);
-
-  const barChartSVGRef = useRef<SVGSVGElement | null>(null);
 
   const result = data?.responseData.reduce((accumulator, currentValue) => ({
     ordinaryM: accumulator.ordinaryM + parseInt(currentValue.household_ordinary_m, 10),
@@ -38,24 +53,85 @@ function ResultPage() {
 
   if (isLoading) return <div />;
 
-  const barChartSVG = select(barChartSVGRef.current);
-  barChartSVG.selectAll('circle')
-    .data(svgData)
-    .join(
-      (enter) => enter.append('circle')
-        .attr('class', 'new')
-        .attr('r', (value) => value)
-        .attr('cx', (value) => value * 2)
-        .attr('cy', (value) => value * 2)
-        .attr('stroke', 'red'),
-      (update) => update.attr('class', 'updated'),
-      (exit) => exit.remove(),
-    );
+  const barChartData = {
+    labels: ['共同生活', '獨立生活'],
+    datasets: [
+      {
+        data: [result?.ordinaryM, result?.singleM],
+        label: '男性',
+        backgroundColor: '#7C5FB1',
+        fill: true,
+        categoryPercentage: 0.3,
+        barPercentage: 0.8,
+      },
+      {
+        data: [result?.ordinaryF, result?.singleF],
+        label: '女性',
+        backgroundColor: '#C29EFE',
+        fill: true,
+        categoryPercentage: 0.3,
+        barPercentage: 0.8,
+      },
+    ],
+  };
 
   return (
     <div className="mt-12 flex w-full flex-col items-center gap-12">
       <div className="text-[25px]">{`${year}年 ${county} ${town}`}</div>
-      <svg ref={barChartSVGRef} className="h-[60px] w-[60px] bg-gray-200" />
+      <div className="text-center text-3xl">人口數統計</div>
+      <div className="-mb-10 w-full">數量</div>
+      <div className="flex h-fit w-[95%] justify-center sm:w-full">
+        <Bar
+          data={barChartData}
+          plugins={[ChartDataLabels]}
+          options={{
+            plugins: {
+              datalabels: {
+                font: {
+                  size: 10,
+                },
+                anchor: 'end',
+                align: 'top',
+                formatter(value:number) {
+                  return value.toLocaleString();
+                },
+              },
+              legend: {
+                position: 'bottom',
+                labels: {
+                  usePointStyle: true,
+                  pointStyle: 'circle',
+                },
+              },
+            },
+            scales: {
+              y: {
+                ticks: {
+                  callback(label:number | string) {
+                    return `${label as number / 1000}k`;
+                  },
+                },
+                border: {
+                  display: false,
+                },
+              },
+              x: {
+                title: {
+                  display: true,
+                  text: '型態',
+                  font: {
+                    size: 16,
+                  },
+                },
+                grid: {
+                  display: false,
+                },
+              },
+
+            },
+          }}
+        />
+      </div>
       <div>{`共同生活_男: ${result?.ordinaryM}`}</div>
       <div>{`共同生活_女: ${result?.ordinaryF}`}</div>
       <div>{`獨立生活_男: ${result?.singleM}`}</div>
